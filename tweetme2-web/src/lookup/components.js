3f4c2a249afdfc5605ad3a04e374dbe7e65f4e61
sai-myne <ahlusir@gmail.com>
2020-06-18 17:10:36 +0800

    update - 69 Handling New Tweet

--- tweetme2-web/src/lookup/components.js
+++ tweetme2-web/src/lookup/components.js
@@ -25,11 +25,11 @@ function lookup(method, endpoint, callback, data) {
   const csrftoken = getCookie('csrftoken');
   xhr.open(method, url)
   xhr.setRequestHeader("Content-Type", "application/json")
-  //xhr.setRequestHeader("HTTP_X_REQUESTED_WITH", "XMLHttpRequest")
-  xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest")
+  
+  
   if (csrftoken){
-    
-    
+    //xhr.setRequestHeader("HTTP_X_REQUESTED_WITH", "XMLHttpRequest")    
+    xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest")
     xhr.setRequestHeader("X-CSRFToken", csrftoken)
   }
   
