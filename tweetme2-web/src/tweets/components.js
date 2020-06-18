81a1856133d314992c2bc47225207f70d3d8b3f6
sai-myne <ahlusir@gmail.com>
2020-06-18 10:52:17 +0800

    update- 59 Improved Action Btn

--- tweetme2-web/src/tweets/components.js
+++ tweetme2-web/src/tweets/components.js
@@ -1,32 +1,46 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
-import {loadTweets} from '../lookup'
-export function TweetsList(props){
-    const [tweets, setTweets] = useState([])
-    
-    useEffect(() => {
-      const myCallback = (response, status) => {
-        if(status === 200)
-          setTweets(response)
-        else {
-          alert("There was an error")
-        } 
+import { loadTweets } from "../lookup";
+export function TweetsList(props) {
+  const [tweets, setTweets] = useState([]);
+
+  useEffect(() => {
+    const myCallback = (response, status) => {
+      if (status === 200) setTweets(response);
+      else {
+        alert("There was an error");
       }
-      loadTweets(myCallback)
-    }, [])
-    return tweets.map((item, index) => {
-      return <Tweet tweet={item} key={`${index}-{item.id}`} className='my-5 py-5 border bg-white text-dark'/>
-    })
-  }
+    };
+    loadTweets(myCallback);
+  }, []);
+  return tweets.map((item, index) => {
+    return (
+      <Tweet
+        tweet={item}
+        key={`${index}-{item.id}`}
+        className="my-5 py-5 border bg-white text-dark"
+      />
+    );
+  });
+}
 
 export function ActionBtn(props) {
   const { tweet, action } = props;
   const className = props.className
     ? props.className
     : "btn btn-primary btn-sm";
-  return action.type === "like" ? (
-    <button className={className}>{tweet.likes} likes</button>
-  ) : null;
+  const actionDisplay = action.display ? action.display : "Action"
+  let likes = tweet.likes
+  const handleClick = (event) => {
+    event.preventDefault();
+    if(action.type === 'like'){
+        console.log(tweet.likes + 1)
+        likes = tweet.likes + 1
+    }
+}
+  const display = action.type === 'like' ? `${likes} ${actionDisplay}` : actionDisplay
+  
+  return <button className={className} onClick={handleClick}>{display}</button>;
 }
 
 export function Tweet(props) {
@@ -41,8 +55,9 @@ export function Tweet(props) {
         {tweet.id} - {tweet.content}
       </p>
       <div className="btn btn-group">
-        <ActionBtn tweet={tweet} action={{ type: "like" }} />
-        <ActionBtn tweet={tweet} action={{ type: "unlike" }} />
+        <ActionBtn tweet={tweet} action={{ type: "like", display: "Likes" }} />
+        <ActionBtn tweet={tweet} action={{ type: "unlike", display: "Unlike" }} />
+        <ActionBtn tweet={tweet} action={{ type: "retweet", display: ""}} />
       </div>
     </div>
   );
