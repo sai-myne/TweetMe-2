fe32e11d765e056d07762f3b53fa225ec9437a8f
sai-myne <ahlusir@gmail.com>
2020-06-19 16:43:17 +0800

    update - 84 Authentication & Registration

--- tweetme2-web/src/tweets/lookup.js
+++ tweetme2-web/src/tweets/lookup.js
@@ -1,16 +1,16 @@
-import { backendlookup } from "../lookup";
+import { backendLookup } from "../lookup";
 
 export function apiTweetCreate(newTweet, callback) {
-  backendlookup("POST", "/tweets/create/", callback, { content: newTweet });
+  backendLookup("POST", "/tweets/create/", callback, { content: newTweet });
 }
 
 export function apiTweetAction(tweetId, action, callback) {
   const data = { id: tweetId, action: action };
-  backendlookup("POST", "/tweets/action/", callback, data);
+  backendLookup("POST", "/tweets/action/", callback, data);
 }
 
 export function apiTweetDetail(tweetId, callback) {
-  backendlookup("GET", `/tweets/${tweetId}/`, callback);
+  backendLookup("GET", `/tweets/${tweetId}/`, callback);
 }
 
 export function apiTweetList(username, callback) {
@@ -18,5 +18,5 @@ export function apiTweetList(username, callback) {
     if(username){
         endpoint = `/tweets/?username=${username}`
     }
-  backendlookup("GET", endpoint, callback);
+    backendLookup("GET", endpoint, callback);
 }
