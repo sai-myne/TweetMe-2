7b54b01df565c657b1ab1d62b58aedbb68c2456d
sai-myne <ahlusir@gmail.com>
2020-06-19 15:40:54 +0800

    update - 83 Login Required Redirect

--- tweetme2-web/src/lookup/components.js
+++ tweetme2-web/src/lookup/components.js
@@ -34,10 +34,15 @@ export function backendlookup(method, endpoint, callback, data) {
   }
   
   xhr.onload = function() {
+    if(xhr.status === 403){
+      const detail = xhr.response.detail
+      if(detail === "Authentication credentials were not provided."){
+        window.location.href = "/login?showLoginRequired=true"
+      }
+    }
     callback(xhr.response, xhr.status)
   }
   xhr.onerror = function (e) {
-    console.log(e)
     callback({"message": "The request was an error"}, 400)
   }
   xhr.send(jsonData)
