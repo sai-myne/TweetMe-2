e07265c705ae6fbf48841a9c63cf7156c174d88b
sai-myne <ahlusir@gmail.com>
2020-06-21 16:26:07 +0800

    update - 108 Display Follower Count with Numeraljs

--- tweetme2-web/src/profiles/badge.js
+++ tweetme2-web/src/profiles/badge.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
-
 import { apiProfileDetail, apiProfileFollowToggle } from "./lookup";
 
 import { UserDisplay, UserPicture } from "./components";
 
+import {DisplayCount} from './util'
+
 function ProfileBadge(props) {
   const { user, didFollowToggle, profileLoading } = props;
-  let currentVerb = (user && user.is_following) ? "Unfollow" : "Follow";
-  currentVerb = profileLoading ? "Loading..." : currentVerb
+  let currentVerb = user && user.is_following ? "Unfollow" : "Follow";
+  currentVerb = profileLoading ? "Loading..." : currentVerb;
   const handleFollowToggle = (event) => {
     event.preventDefault();
     if (didFollowToggle && !profileLoading) {
@@ -17,7 +18,18 @@ function ProfileBadge(props) {
   return user ? (
     <div>
       <UserPicture user={user} hideLink />
-      <p><UserDisplay user={user} includeFullName hideLink /></p>
+      <p>
+        <UserDisplay user={user} includeFullName hideLink />
+      </p>
+      <p>
+        <DisplayCount>{user.follower_count}</DisplayCount>{" "}
+        {user.follower_count === 1 ? "follower" : "followers"}
+      </p>
+      <p>
+        <DisplayCount>{user.following_count}</DisplayCount> following
+      </p>
+      <p>{user.location}</p>
+      <p>{user.bio}</p>
       <button className="btn btn-primary" onClick={handleFollowToggle}>
         {currentVerb}
       </button>
@@ -45,13 +57,13 @@ export function ProfileBadgeComponent(props) {
 
   const handleNewFollow = (actionVerb) => {
     apiProfileFollowToggle(username, actionVerb, (response, status) => {
-        // console.log(response, status)
-        if (status === 200) {
-            setProfile(response)
-            // apiProfileDetail(username, handleBackendLookup);
-        }
-        setProfileLoading(false)
-    })
+      // console.log(response, status)
+      if (status === 200) {
+        setProfile(response);
+        // apiProfileDetail(username, handleBackendLookup);
+      }
+      setProfileLoading(false);
+    });
     setProfileLoading(true);
   };
   return didLookup === false ? (
